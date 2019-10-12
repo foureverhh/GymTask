a8d807a7b53490c7493f018313a8974c620ae729
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-10-12 13:16:51 +0200

    Add test case to check whether file could be successfully read in

--- src/gym/MemberData.java
+++ src/gym/MemberData.java
@@ -1,7 +1,27 @@
 package gym;
 
-public class MemberData {
-    public void readMemberDate(){
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class MemberData {
+    public static void main(String[] args) {
+        Path memberList = Paths.get("files/members.txt");
+        readMemberData(memberList);
+    }
+    public static void readMemberData(Path filePath){
+        try(BufferedReader reader = Files.newBufferedReader(filePath)){
+            String content = null;
+            //String[] IdAndNames = new String[2];
+            while ((content=reader.readLine())!=null){
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
